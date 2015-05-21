cac86181b7aac98713d056d2a7ffb56e7e0829ae
Dmitry Anshilevich <dimaaan@gmail.com>
2015-05-21 16:59:53 +0300

    implement VerbAttribute

--- NOpt/VerbAttribute.cs
+++ NOpt/VerbAttribute.cs
@@ -2,11 +2,27 @@
 
 namespace NOpt
 {
+    /// <summary>
+    /// A command to execute.
+    /// Verb must be first argument.
+    /// <example>
+    /// "git commit". Commit is a verb.
+    /// Other examples: 
+    /// program save -f file
+    /// program load -f file
+    /// "save" and "load" is a verbs
+    /// </example>
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, 
         AllowMultiple = false, 
         Inherited = true)]
     public sealed class VerbAttribute : Attribute
     {
+        public VerbAttribute(string name)
+        {
+            Name = name;
+        }
 
+        public string Name { get; set; }
     }
 }
