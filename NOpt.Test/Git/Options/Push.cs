4c89a80fadf96e518dbe52701d0fd50040fbf44f
Dmitry Anshilevich <dimaaan@gmail.com>
2015-05-24 02:36:32 +0300

    add push options

---
+++ NOpt.Test/Git/Options/Push.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NOpt.Test.Git.Options
+{
+    /*
+    git push    [--all | --mirror | --tags] [--follow-tags] [-n | --dry-run] [--receive-pack=<git-receive-pack>]
+                [--repo=<repository>] [-f | --force] [--prune] [-v | --verbose] [-u | --set-upstream]
+                [--force-with-lease[=<refname>[:<expect>]]]
+                [--no-verify] [<repository> [<refspec>…]]
+    /*/
+    public class Push
+    {
+        // TODO implement
+    }
+}
