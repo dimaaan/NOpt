acd90016ce375d3fd58d4a639885138b9407f972
Dmitry Anshilevich <dimaaan@gmail.com>
2015-05-24 02:37:06 +0300

    add verbs to options

--- NOpt.Test/Git/Options/Options.cs
+++ NOpt.Test/Git/Options/Options.cs
@@ -66,6 +66,106 @@ namespace NOpt.Test.Git.Options
         [Verb("am")]
         public Am am;
 
-        // TODO command <args>
+        [Verb("archive")]
+        public Archive archive;
+
+        [Verb("bisect")]
+        public Bisect bisect;
+
+        [Verb("branch")]
+        public Branch branch;
+
+        [Verb("bundle")]
+        public Bundle bundle;
+
+        [Verb("checkout")]
+        public Checkout checkout;
+
+        [Verb("cherry-pick")]
+        public CherryPick CherryPick;
+
+        [Verb("citool")]
+        public Citool citool;
+
+        [Verb("clean")]
+        public Clean clean;
+
+        [Verb("clone")]
+        public Clone Clone;
+
+        [Verb("commit")]
+        public Commit commit;
+
+        [Verb("describe")]
+        public Describe describe;
+
+        [Verb("diff")]
+        public Diff diff;
+
+        [Verb("fetch")]
+        public Fetch fetch;
+
+        [Verb("format-patch")]
+        public FormatPatch formatPatch;
+
+        [Verb("gc")]
+        public Gc gc;
+
+        [Verb("grep")]
+        public Grep grep;
+
+        [Verb("gui")]
+        public Gui gui;
+
+        [Verb("init")]
+        public Init init;
+
+        [Verb("log")]
+        public Log log;
+
+        [Verb("merge")]
+        public Merge merge;
+
+        [Verb("mv")]
+        public Mv mv;
+
+        [Verb("notes")]
+        public Mv notes;
+
+        [Verb("pulll")]
+        public Pull pull;
+
+        [Verb("push")]
+        public Push push;
+
+        [Verb("rebase")]
+        public Rebase rebase;
+
+        [Verb("reset")]
+        public Reset reset;
+
+        [Verb("revert")]
+        public Revert revert;
+
+        [Verb("rm")]
+        public Rm rm;
+
+        [Verb("shortlog")]
+        public ShortLog shortLog;
+
+        [Verb("show")]
+        public Show show;
+
+        [Verb("stash")]
+        public Stash stash;
+
+        [Verb("status")]
+        public Status status;
+
+        [Verb("submodule")]
+        public Submodule submodule;
+
+        [Verb("tag")]
+        public Tag tag;
     }
 }
