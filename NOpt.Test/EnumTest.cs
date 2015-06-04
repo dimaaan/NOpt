9b6ffc2f016eeda5284dc22c5815bea0e343ee1f
Dmitry Anshilevich <dimaaan@gmail.com>
2015-06-04 20:35:03 +0300

    add enum tests

--- NOpt.Test/EnumTest.cs
+++ NOpt.Test/EnumTest.cs
@@ -14,9 +14,10 @@ namespace NOpt.Test
         {
             public class Options
             {
+                [Flags]
                 public enum Actions
                 {
-                    READ, WRITE
+                    READ = 1, WRITE = 2, READ_WRITE = 4
                 }
 
                 [Value(0)]
@@ -26,10 +27,36 @@ namespace NOpt.Test
             [Fact]
             public void check()
             {
-                Options opt = NOpt.Parse<Options>(new string[] { "WRITE" });
+                Options opt;
+                
+                opt = NOpt.Parse<Options>(new string[] { "WRITE" });
                 Assert.Equal(Options.Actions.WRITE, opt.actions);
 
-                Assert.Throws<FormatException>(() => NOpt.Parse<Options>(new string[] { "QQQ" }));
+                opt = NOpt.Parse<Options>(new string[] { "WrItE" });
+                Assert.Equal(Options.Actions.WRITE, opt.actions);
+
+                opt = NOpt.Parse<Options>(new string[] { "read_write" });
+                Assert.Equal(Options.Actions.READ_WRITE, opt.actions);
+
+                opt = NOpt.Parse<Options>(new string[] { "READ_WRITE" });
+                Assert.Equal(Options.Actions.READ_WRITE, opt.actions);
+
+                opt = NOpt.Parse<Options>(new string[] { "READ-WRITE" });
+                Assert.Equal(Options.Actions.READ_WRITE, opt.actions);
+
+                opt = NOpt.Parse<Options>(new string[] { "read-WRITE" });
+                Assert.Equal(Options.Actions.READ_WRITE, opt.actions);
+
+                opt = NOpt.Parse<Options>(new string[] { "read-write" });
+                Assert.Equal(Options.Actions.READ_WRITE, opt.actions);
+
+                opt = NOpt.Parse<Options>(new string[] { "read,write" });
+                Assert.Equal(Options.Actions.READ | Options.Actions.WRITE, opt.actions);
+
+                opt = NOpt.Parse<Options>(new string[] { "read,write,read-write" });
+                Assert.Equal(Options.Actions.READ | Options.Actions.WRITE | Options.Actions.READ_WRITE, opt.actions);
+
+                Assert.Throws<FormatException>(() => NOpt.Parse<Options>(new string[] { "XXX" }));
             }
         }
     }
