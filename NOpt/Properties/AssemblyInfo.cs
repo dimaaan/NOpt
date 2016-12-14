eabd56fa4f44ba6b672c791d89686b6dcf3b686b
dimaaan <dimaaan@gmail.com>
2016-12-14 16:48:28 +0300

    update copyright

--- NOpt/Properties/AssemblyInfo.cs
+++ NOpt/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NOpt")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
